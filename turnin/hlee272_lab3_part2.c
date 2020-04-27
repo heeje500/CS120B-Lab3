78a0d3d866328fc83cfc375602c2f91a76dc0c97
heeje500 <63324000+heeje500@users.noreply.github.com>
2020-04-26 18:09:19 -0700

    Update hlee272_lab3_part2.c

--- turnin/hlee272_lab3_part2.c
+++ turnin/hlee272_lab3_part2.c
@@ -1,7 +1,7 @@
 /*	Author: hlee272
  *  Partner(s) Name: 
  *	Lab Section:
- *	Assignment: Lab #3  Exercise #2
+ *	Assignment: Lab #5  Exercise #1
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
